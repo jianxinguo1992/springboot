b2b31ded148232f7c82587ee98011582becdb874
jianxinGuo <guojianxin1992@foxmail.com>
2017-11-04 00:47:05 +0800

    Initial commit

---
+++ src/main/java/jxguo/study/demo/SpringbootApplication.java
@@ -0,0 +1,12 @@
+package jxguo.study.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
